b9aca990b44a7fbcc142401af152186051731af8
AndreaMarchica <andrea.marchica94@gmail.com>
2024-01-29 10:38:06 +0100

    Create project folder

---
+++ src/main/java/BuildWeek2Team1/AziendaDiEnergia/AziendaDiEnergiaApplication.java
@@ -0,0 +1,13 @@
+package BuildWeek2Team1.AziendaDiEnergia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AziendaDiEnergiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AziendaDiEnergiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BuildWeek2Team1/AziendaDiEnergia/AziendaDiEnergiaApplicationTests.java
@@ -0,0 +1,13 @@
+package BuildWeek2Team1.AziendaDiEnergia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AziendaDiEnergiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
