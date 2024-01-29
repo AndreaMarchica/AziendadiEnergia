77793f894fbdb754101447c9b805b8362ef1c897
Lauragallace <143189799+Lauragallace@users.noreply.github.com>
2024-01-29 15:29:32 +0100

    -

--- src/main/java/BuildWeek2Team1/AziendaDiEnergia/exceptions/BadRequestException.java
+++ src/main/java/BuildWeek2Team1/AziendaDiEnergia/exceptions/BadRequestException.java
@@ -7,25 +7,13 @@ import java.util.List;
 
 @Getter
 public class BadRequestException extends RuntimeException {
-<<<<<<< HEAD
     private List<ObjectError> errors;
-=======
-    private List<ObjectError> errorsList;
->>>>>>> develop
 
     public BadRequestException(String message) {
         super(message);
     }
 
-<<<<<<< HEAD
     public BadRequestException(List<ObjectError> errors) {
         this.errors = errors;
     }
-=======
-    public BadRequestException(List<ObjectError> errorsList) {
-        super("Errori nel body");
-        this.errorsList = errorsList;
-    }
-
->>>>>>> develop
 }
--- src/main/java/BuildWeek2Team1/AziendaDiEnergia/exceptions/NotFoundException.java
+++ src/main/java/BuildWeek2Team1/AziendaDiEnergia/exceptions/NotFoundException.java
@@ -1,23 +1,7 @@
 package BuildWeek2Team1.AziendaDiEnergia.exceptions;
 
-<<<<<<< HEAD
-
 public class NotFoundException extends RuntimeException {
     public NotFoundException(long idNumero) {
         super("Elemento con id " + idNumero + " non trovato");
     }
-
-
-=======
-import java.util.UUID;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-
-    public NotFoundException(UUID id) {
-        super(id + " non trovato!");
-    }
->>>>>>> develop
 }
