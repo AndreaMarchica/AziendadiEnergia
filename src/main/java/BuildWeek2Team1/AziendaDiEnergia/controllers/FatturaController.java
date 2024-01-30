e5058f496ad02056484d605e0b3ea425a73ca5d4
Lauragallace <143189799+Lauragallace@users.noreply.github.com>
2024-01-30 12:00:29 +0100

    .

--- src/main/java/BuildWeek2Team1/AziendaDiEnergia/controllers/FatturaController.java
+++ src/main/java/BuildWeek2Team1/AziendaDiEnergia/controllers/FatturaController.java
@@ -1,11 +1,13 @@
 package BuildWeek2Team1.AziendaDiEnergia.controllers;
 
+
 import BuildWeek2Team1.AziendaDiEnergia.entities.Fattura;
-import BuildWeek2Team1.AziendaDiEnergia.exceptions.BadRequestException;
 import BuildWeek2Team1.AziendaDiEnergia.payloads.FatturaPostDTO;
+import BuildWeek2Team1.AziendaDiEnergia.payloads.FatturaPutDTO;
 import BuildWeek2Team1.AziendaDiEnergia.services.ClienteService;
 import BuildWeek2Team1.AziendaDiEnergia.services.FatturaService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -13,6 +15,8 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/fatture")
@@ -24,15 +28,15 @@ public class FatturaController {
     private ClienteService clienteService;
 
 
-    @PostMapping("")
+/*    @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
-    public Fattura saveFattura(@RequestBody @Validated FatturaPostDTO body, BindingResult validation) {
+    public Fattura saveFattura(@RequestBody @Validated FatturaPostDTO body, BindingResult validation) throws Exception {
         if (validation.hasErrors()) {
-            throw new BadRequestException(validation.getAllErrors());
+            throw new BadRequestException(String.valueOf(validation.getAllErrors()));
         } else {
             return fatturaService.save(body);
         }
-    }
+    }*/
 
     @GetMapping("")
     public Page<Fattura> getFatturaByFiltro(
@@ -40,7 +44,7 @@ public class FatturaController {
             @RequestParam(defaultValue = "") String statoFattura,
             @RequestParam(defaultValue = "") String data,
             @RequestParam(defaultValue = "0") int anno,
-            @RequestParam(defaultValue = "0") long clientId,
+            @RequestParam(defaultValue = "") UUID clientId,
             @RequestParam(defaultValue = "0") double importoMin,
             @RequestParam(defaultValue = "0") double importoMax,
             @RequestParam(defaultValue = "0") int page,
@@ -50,23 +54,23 @@ public class FatturaController {
     }
 
     @GetMapping("/{idNumero}")
-    public Fattura findByIdNumero(@PathVariable long idNumero) {
+    public Fattura findByIdNumero(@PathVariable UUID idNumero) {
         return fatturaService.findById(idNumero);
     }
 
-    @PutMapping("/{idNumero}")
-    public Fattura findAndUpdateById(@PathVariable long idNumero, @RequestBody @Validated FatturaPutDTO body, BindingResult validation) {
+/*    @PutMapping("/{idNumero}")
+    public Fattura findAndUpdateById(@PathVariable UUID idNumero, @RequestBody @Validated FatturaPutDTO body, BindingResult validation) throws Exception {
         if (validation.hasErrors()) {
-            throw new BadRequestException(validation.getAllErrors());
+            throw new BadRequestException(String.valueOf(validation.getAllErrors()));
         } else {
             return fatturaService.findAndUpdateById(idNumero, body);
         }
-    }
+    }*/
 
     @DeleteMapping("/{idNumero}")
     @PreAuthorize("hasAuthority('ADMIN')")
     @ResponseStatus(HttpStatus.NO_CONTENT) // <-- 204 NO CONTENT
-    public void findAndDeleteByIdNumero(@PathVariable long idNumero) {
+    public void findAndDeleteByIdNumero(@PathVariable UUID idNumero) {
         fatturaService.findAndDeleteByIdNumero(idNumero);
     }
 }
